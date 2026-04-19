d66eaaaa91065008e239c82b5c3738ee61b43efa
Joalor64 <rhgarcon@gmail.com>
2026-04-19 15:51:30 -0400

    path fix

--- extension/videoview/VideoView.hx
+++ extension/videoview/VideoView.hx
@@ -3,9 +3,14 @@ package extension.videoview;
 #if android
 import lime.system.JNI;
 #end
+import sys.FileSystem;
+import sys.io.File;
+import openfl.utils.Assets;
+import haxe.io.Path;
 
 /**
  * @author Saw (M.A. Jigsaw)
+ * Modified by Joalor64
  */
 class VideoView
 {
@@ -14,12 +19,37 @@ class VideoView
 
 	public static function playVideo(path:String = null):Void
 	{
+		var finalPath:String = resolvePath(path);
+
 		#if android
 		var _callbackFunc = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
 		var _playVideo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "playVideo", "(Ljava/lang/String;)V");
 
 		_callbackFunc(new CallBack());
-		_playVideo(path);
+		_playVideo(finalPath);
+		#end
+	}
+
+	private static function resolvePath(path:String):String
+	{
+		if (StringTools.startsWith(path, "http"))
+			return path;
+
+		#if android
+		var libraryPath = path;
+		var destination = Path.join([Sys.getCwd(), Path.withoutDirectory(path)]);
+
+		if (!FileSystem.exists(destination))
+		{
+			var bytes = Assets.getBytes(libraryPath);
+			if (bytes != null)
+			{
+				File.saveBytes(destination, bytes);
+			}
+		}
+		return destination;
+		#else
+		return path;
 		#end
 	}
 }
